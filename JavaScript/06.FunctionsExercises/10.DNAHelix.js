d66923c865234e571b9b161e39cf0828669d4ca1
jicata <jitza_nest@yahoo.com>
2017-02-26 19:49:11 +0200

    multidimensional arrays

--- JavaScript/06.FunctionsExercises/10.DNAHelix.js
+++ JavaScript/06.FunctionsExercises/10.DNAHelix.js
@@ -1,8 +1,8 @@
 /*jshint esversion: 6 */
 function dnaHelix(input) {
     let helixLength = Number(input);
-    var helixPairs = ["AT","CG", "TT", "AG", "GG"];
-    var helixSequence =["**#1##2#**", "*#1#--#2#*", "#1#----#2#","*#1#--#2#*"];
+    let helixPairs = ["AT","CG", "TT", "AG", "GG"];
+    let helixSequence =["**#1##2#**", "*#1#--#2#*", "#1#----#2#","*#1#--#2#*"];
     let pairCounter =0;
     let sequenceCounter = 0;
     for(let i = 0; i<helixLength;i++){
