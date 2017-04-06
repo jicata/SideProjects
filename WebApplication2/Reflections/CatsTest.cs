7b86b0064be9092c36293a46d39f0bdb299bce16
Svetlin Galov <svetlin.galov@gmail.com>
2017-04-06 18:16:30 +0300

    added ASP NE MVC testing

--- WebApplication2/Reflections/CatsTest.cs
+++ WebApplication2/Reflections/CatsTest.cs
@@ -1,17 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Data.Entity;
 using System.Linq;
+using System.Reflection;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Moq;
 using NUnit.Framework;
+using WebApplication2;
 using WebApplication2.Models;
 using WebApplication2.Models.Enums;
+
 
 namespace Reflections
 {
     [TestFixture]
     public class CatsTest
     {
+        private Mock<HttpContextBase> moqContext;
+        private Mock<HttpRequestBase> moqRequest;
+        private NameValueCollection formValues;
+
+        [OneTimeSetUp]
+        public void SetupTests()
+        {
+            moqContext = new Mock<HttpContextBase>();
+            moqRequest = new Mock<HttpRequestBase>();
+            moqContext.Setup(x => x.Request).Returns(moqRequest.Object);
+            formValues = new NameValueCollection
+            {
+                { "FirstName", "Jonathan" },
+                { "LastName", "Danylko" }
+            };
+        }
+        public Type ContextType { get; set; }
+
+        private void FindTheTypeOfDb()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var referenced = assembly.GetReferencedAssemblies();
+
+            foreach (var assemblyName in referenced)
+            {
+                if (!Assembly.Load(assemblyName).GlobalAssemblyCache)
+                {
+                    assembly = Assembly.Load(assemblyName);
+                }
+            }
+            var types = assembly.GetTypes();
+            Type dbType = null;
+            foreach (Type type in types)
+            {
+
+                if (type.IsSubclassOf(typeof(DbContext)))
+                {
+                    dbType = type;
+                    break;
+                }
+            }
+
+            object dbcontext = Activator.CreateInstance(dbType);
+            dynamic context = dbcontext;
+        }
         [TestCase]
         public void TestCatsIndex()
         {
@@ -50,7 +102,35 @@ namespace Reflections
             var cats = (controller.Index() as ViewResult).Model;
             // Assert.IsInstanceOf<ViewResult>(cats);
             Assert.IsInstanceOf<IEnumerable<Cat>>(cats);
-            Assert.AreEqual(data,cats);
+            Assert.AreEqual(data, cats);
+        }
+
+        [TestCase]
+        public void MoqRoutingTest()
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            moqRequest.Setup(e => e.AppRelativeCurrentExecutionFilePath).Returns("~/Home/Index");
+            // Act
+            RouteData routeData = routes.GetRouteData(moqContext.Object);
+            // Assert
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("Home", routeData.Values["controller"]);
+            Assert.AreEqual("Index", routeData.Values["action"]);
+        }
+
+        [TestCase]
+        public void MoqFormsTest()
+        {
+            // Arrange            
+            moqRequest.Setup(r => r.Form).Returns(formValues);
+            // Act
+            var forms = moqContext.Object.Request.Form;
+            // Assert
+            Assert.IsNotNull(forms);
+            Assert.AreEqual("Jonathan", forms["FirstName"]);
+            Assert.AreEqual("Danylko", forms["LastName"]);
         }
     }
 }
--- WebApplication2/Reflections/Program.cs
+++ WebApplication2/Reflections/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -15,11 +16,20 @@ namespace Reflections
     {
         static void Main(string[] args)
         {
-            var assembly = Assembly.GetExecutingAssembly();
+
+            string workingDir = @"C:\ASP-Projects\ShishaKingdom";
+            string pattern = @"*.csproj";
+
+            var result = FindFirstFileMatchingPattern(workingDir, pattern);
+            string dllPath = @"C:\ASP-Projects\ShishaKingdom\ShishaKingdom.Web\bin\ShishaKingdom.Web.dll";
+
+
+            var assembly = Assembly.LoadFrom(dllPath);
             var referenced = assembly.GetReferencedAssemblies();
             
             foreach (var assemblyName in referenced)
             {
+                
                 if (!Assembly.Load(assemblyName).GlobalAssemblyCache)
                 {
                   assembly = Assembly.Load(assemblyName);
@@ -38,14 +48,48 @@ namespace Reflections
             }
 
             object dbcontext = Activator.CreateInstance(dbType);
-            dynamic context = dbcontext;
-            DbSet<Cat> cats = context.Cats;
-
-            var catsList = cats.ToList();
-            foreach (var cat in catsList)
+            var properties = dbcontext.GetType().GetProperties();
+            string[] expectedModels = new[] {"Products", "Categories"};
+            foreach (var propertyInfo in properties)
             {
-                Console.WriteLine(cat.Name);
+                var propertyType = propertyInfo.PropertyType.Name.Contains("DbSet");
+                //Console.WriteLine(propertyInfo.PropertyType.Name);
+                if (propertyType && expectedModels.Any(m=>m.Contains(propertyInfo.Name)))
+                {
+                    var isGeneric = propertyInfo.PropertyType.IsGenericType;
+                    Type t = propertyInfo.PropertyType.GetGenericArguments()[0];
+                    foreach (var property in t.GetProperties())
+                    {
+                        if (!property.PropertyType.IsPrimitive)
+                        {
+                            Console.WriteLine(property.PropertyType);
+                        }
+                       
+                    }
+                }
             }
+
         }
+        public static string FindFirstFileMatchingPattern(string workingDirectory, string pattern)
+        {
+            var files = new List<string>(
+                Directory.GetFiles(
+                    workingDirectory,
+                    pattern,
+                    SearchOption.AllDirectories));
+            if (files.Count == 0)
+            {
+                throw new ArgumentException(
+                    $"'{pattern}' file not found in output directory!",
+                    nameof(pattern));
+            }
+            if (files.Count > 1)
+            {
+                files[0] = files.OrderByDescending(f => new FileInfo(f).Length).FirstOrDefault();
+            }
+
+            return files[0];
+        } 
     }
+
 }
